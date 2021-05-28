59e63ce5d8b1c832ec616972ea75ee18764eb215
Seba Gonzalez <sebastian.gonzalez@pedidosya.com>
2021-05-28 18:01:15 -0300

    iteracion 1

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,81 @@
-describe("a test suite", () => {
-  it("should fail", () => {
-    expect(1).toBe(1);
+import { Character, Game, MAX_HEALTH, MIN_HEALTH } from "./index";
+
+describe("When a character is created", () => {
+  it("should have MAX of health", () => {
+    const aCharacter = new Character();
+    expect(aCharacter.health).toBe(MAX_HEALTH);
+  });
+
+  it("should start at level 1", () => {
+    const aCharacter = new Character();
+    expect(aCharacter.level).toBe(1);
+  });
+
+  it("should start alive", () => {
+    const aCharacter = new Character();
+    expect(aCharacter.isAlive()).toBeTruthy();
+  });
+});
+
+describe("When a character receives damage", () => {
+  it("should reduce its health", () => {
+    const damage = MAX_HEALTH / 2;
+    const game = new Game();
+    const attackingCharacter = new Character();
+    const attackedCharacter = new Character();
+
+    game.dealDamage(attackingCharacter, attackedCharacter, damage);
+
+    expect(attackedCharacter.health).toBe(MAX_HEALTH - damage);
+  });
+
+  it("should die if its health becomes the MIN_HEALTH", () => {
+    const damage = MAX_HEALTH * 2;
+    const game = new Game();
+    const attackingCharacter = new Character();
+    const attackedCharacter = new Character();
+
+    game.dealDamage(attackingCharacter, attackedCharacter, damage);
+
+    expect(attackedCharacter.health).toBe(MIN_HEALTH);
+    expect(attackedCharacter.isAlive()).toBeFalsy();
+  });
+});
+
+describe("When a characters heals a character", () => {
+  it("should increase the healed character health", () => {
+    const health = MAX_HEALTH / 4;
+    const game = new Game();
+    const healerCharacter = new Character();
+    const healedCharacter = new Character();
+    healedCharacter.health = MAX_HEALTH / 2;
+
+    game.heal(healerCharacter, healedCharacter, health);
+
+    expect(healedCharacter.health).toBe(500 + health);
+  });
+
+  it("should NOT heal over max health", () => {
+    const health = MAX_HEALTH * 2;
+    const game = new Game();
+    const healerCharacter = new Character();
+    const healedCharacter = new Character();
+    healedCharacter.health = MAX_HEALTH / 2;
+
+    game.heal(healerCharacter, healedCharacter, health);
+
+    expect(healedCharacter.health).toBe(MAX_HEALTH);
+  });
+
+  it("should NOT heal dead characters", () => {
+    const health = MAX_HEALTH * 2;
+    const game = new Game();
+    const healerCharacter = new Character();
+    const healedCharacter = new Character();
+    healedCharacter.health = MIN_HEALTH;
+
+    game.heal(healerCharacter, healedCharacter, health);
+
+    expect(healedCharacter.health).toBe(MIN_HEALTH);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log("Hello TS");
+export const MAX_HEALTH = 1000;
+export const MIN_HEALTH = 0;
+
+export class Character {
+  health: number;
+  level: number;
+
+  constructor() {
+    this.health = MAX_HEALTH;
+    this.level = 1;
+  }
+
+  isAlive(): boolean {
+    return this.health > MIN_HEALTH;
+  }
+
+  receiveDamage(damage: number): void {
+    const hp = this.health - damage;
+    this.health = hp < MIN_HEALTH ? MIN_HEALTH : hp;
+  }
+
+  receiveHealth(health: number) {
+    const hp = this.health + health;
+    this.health = hp > MAX_HEALTH ? MAX_HEALTH : hp;
+  }
+}
+
+export class Game {
+  constructor() {}
+
+  dealDamage(
+    attackingCharacter: Character,
+    attackedCharacter: Character,
+    damage: number
+  ): void {
+    attackedCharacter.receiveDamage(damage);
+  }
+
+  heal(
+    healerCharacter: Character,
+    healedCharacter: Character,
+    health: number
+  ): void {
+    if (healedCharacter.isAlive()) {
+      healedCharacter.receiveHealth(health);
+    }
+  }
+}
