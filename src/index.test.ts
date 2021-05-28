f48dd9631595426d024a761709b9b214130a27fe
ignaciomartin-gonzalez-peya <ignaciomartin.gonzalez@pedidosya.com>
2021-05-28 18:20:00 -0300

    iteration 1

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,51 @@
+import { Character } from './index';
 describe("a test suite", () => {
-  it("should fail", () => {
-    expect(1).toBe(1);
+  it("should success when create Character", () => {
+    const character = new Character();
+    expect(character.health).toEqual(1000);
+    expect(character.level).toEqual(1);
+    expect(character.isAlive()).toEqual(true);
   });
+
+  it("should decrement health when receive damage", () => {
+    const character = new Character();
+
+    character.receiveDamage(500);
+    expect(character.health).toEqual(500); 
+  });
+  it("should die when helth is above zero", () => {
+    const character = new Character();
+
+    character.receiveDamage(1000);
+
+    expect(character.isAlive()).toEqual(false); 
+  });
+
+  it("when damage received exceeds current Health, Health becomes 0", () => {
+    const character = new Character();
+
+    character.receiveDamage(1010);
+
+    expect(character.health).toEqual(0); 
+  });
+
+  it("character damage to other character", () => {
+    const offensive = new Character();
+    const defensive = new Character();
+
+    offensive.doDamage(defensive, 500);
+
+    expect(defensive.health).toEqual(500); 
+  });
+  
 });
+
+
+/* 
+
+ Characters can Deal Damage to Characters.
+
+Damage is subtracted from Health
+When damage received exceeds current Health, Health becomes 0 and the character dies
+
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello TS");
+class Character {
+  health: number;
+  level: number;
+
+
+  constructor() {
+    this.health = 1000;
+    this.level = 1;
+
+  }
+
+  receiveDamage(damage: number) {
+      if (damage > this.health) {
+          this.health = 0
+      } else {
+        this.health -= damage;
+      }
+  }
+
+  isAlive(): Boolean {
+    return this.health > 0;
+  }
+
+  doDamage(defensiveCharacter: Character, damage: number) {
+    defensiveCharacter.receiveDamage(damage)
+  }
+}
+
+class DoDamage {
+
+    invoke(offensive: Character, defensive: Character, damage: number) {
+        defensive.receiveDamage(number)
+    }
+}
+
+export { Character };
+
+
